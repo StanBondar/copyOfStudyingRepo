95d9eea967f8bc8b6aef2e471fc0fd07a4027298
Stan Bondar <bondar.stan@gmail.com>
2019-11-28 15:51:05 +0200

    HW3 added

---
+++ JSHW/HW3/js/hw3.js
@@ -0,0 +1,59 @@
+//1. Если есть блок кода, который нужно использовать несколько раз - самое время вспомнить о функции.
+//Объявив её одни раз - можем вызывать по имени столько, сколько нужно.
+//2. В функцию передаётся аргумент
+
+let firstNumber = +prompt("Enter 1st number below:");
+
+while (Number.isNaN(+firstNumber) || firstNumber === '' || firstNumber === null){
+    firstNumber = +prompt('Enter first number below:');
+}
+
+let secondNumber = +prompt("Enter 2nd number below:");
+
+while (Number.isNaN(+secondNumber) || secondNumber === '' || secondNumber === null){
+    secondNumber = +prompt('Enter second number below:');
+}
+
+let operator = prompt("Enter operator:");
+while (!'+-*/:'.includes(operator)){
+    operator = prompt("Enter operator:");
+}
+
+
+let sumUp = function (first, second) {
+    return first+second;
+}
+
+let substract = function (first, second) {
+    return first-second;
+}
+
+let multiply = function (first, second) {
+    return first*second;
+}
+
+let divide = function (first, second) {
+    return first/second;
+}
+
+switch (operator){
+    case "+" : {
+        alert(`Answer: ${sumUp(firstNumber, secondNumber)}`);
+        break;
+    }
+
+    case "-" : {
+        alert(`Answer: ${substract(firstNumber, secondNumber)}`);
+        break;
+    }
+
+    case "*" : {
+        alert(`Answer: ${multiply(firstNumber, secondNumber)}`);
+        break;
+    }
+
+    case ("/" || ":") : {
+        alert(`Answer: ${divide(firstNumber, secondNumber)}`);
+        break;
+    }
+}
