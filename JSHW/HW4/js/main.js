523d0bf026afb54e81f400cb7fef6a87ae61a2ec
Stan Bondar <bondar.stan@gmail.com>
2019-12-01 16:15:48 +0200

    I've forgot about in, but HW4 added

---
+++ JSHW/HW4/js/main.js
@@ -0,0 +1,47 @@
+"use strict";
+
+// ## Теоретический вопрос
+//
+// 1. Опишите своими словами, что такое метод обьекта
+//
+// ## Задание
+//
+// Реализовать функцию для создания объекта "пользователь".
+//
+//     #### Технические требования:
+// - Написать функцию `createNewUser()`, которая будет создавать и возвращать объект `newUser`.
+// - При вызове функция должна спросить у вызывающего имя и фамилию.
+// - Используя данные, введенные пользователем, создать объект `newUser` со свойствами `firstName` и `lastName`.
+// - Добавить в объект `newUser` метод `getLogin()`, который будет возвращать первую букву имени пользователя, соединенную с фамилией пользователя, все в нижнем регистре (например, `Ivan Kravchenko → ikravchenko`).
+// - Создать пользователя с помощью функции `createNewUser()`. Вызвать у пользователя функцию `getLogin()`. Вывести в консоль результат выполнения функции.
+//
+//     #### Необязательное задание продвинутой сложности:
+//     - Сделать так, чтобы свойства `firstName` и `lastName` нельзя было изменять напрямую. Создать функции-сеттеры `setFirstName()` и `setLastName()`, которые позволят изменить данные свойства.
+//
+//     #### Литература:
+// - [Объекты как ассоциативные массивы](https://learn.javascript.ru/object)
+// - [Object.defineProperty()](https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Object/defineProperty)
+
+//Метод объекта - функция, которая является значением свойства объекта.
+
+function createNewUser() {
+    let newUser = {
+        firstName : prompt('Enter first name below:'),
+        lastName : prompt('Enter last name below: '),
+        getLogin : function () {
+            let login = this.firstName.charAt(0)+this.lastName;
+            return login.toLowerCase();
+        },
+        setFirstName : function (firstName) {
+            this.firstName = firstName;
+        },
+        setLastName : function (lastName) {
+            this.lastName = lastName;
+        }
+    }
+    return newUser;
+}
+
+let newUser = createNewUser();
+
+console.log(newUser.getLogin());
