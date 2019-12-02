0daf2d75c891a9845cf6105aa52190e987a83f12
Stan Bondar <bondar.stan@gmail.com>
2019-12-02 17:52:58 +0200

    Added own realization of filter

--- JSHW/HW6/js/main.js
+++ JSHW/HW6/js/main.js
@@ -19,7 +19,18 @@ function filterBy(array, dataType) {
     return newArray;
 }
 
+function ownFilterBy(array, dataType){
+    let newArray = [];
+    array.forEach(element => {
+        if(typeof element !== dataType){
+            newArray.push(element);
+        }
+    });
+    return newArray;
+}
+
 let array = ['hello', 'world', 23, '23', null];
 let dataType = 'string';
 
 console.log(filterBy(array, dataType));
+console.log(ownFilterBy(array, dataType));
