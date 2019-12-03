10abb029ea0177a43276d341b4fe594444f16902
Stan Bondar <bondar.stan@gmail.com>
2019-12-03 21:34:21 +0200

    Changed hw3.js with proper realization

--- JSHW/HW3/js/hw3.js
+++ JSHW/HW3/js/hw3.js
@@ -2,58 +2,44 @@
 //Объявив её одни раз - можем вызывать по имени столько, сколько нужно.
 //2. В функцию передаётся аргумент, что бы удовлетворить запросы параметра функции.
 
-let firstNumber = +prompt("Enter 1st number below:");
+let firstNumber;
 
-while (Number.isNaN(+firstNumber) || firstNumber === '' || firstNumber === null){
-    firstNumber = +prompt('Enter first number below:');
+while (Number.isNaN(+firstNumber) || !firstNumber){
+    firstNumber = prompt('Enter first number below:', '0');
 }
 
-let secondNumber = +prompt("Enter 2nd number below:");
+let secondNumber;
 
 while (Number.isNaN(+secondNumber) || secondNumber === '' || secondNumber === null){
-    secondNumber = +prompt('Enter second number below:');
+    secondNumber = prompt('Enter second number below:', '0');
 }
 
+const operators = ['+', '-', '*', '/', ':'];
+
 let operator = prompt("Enter operator:");
-while (!'+-*/:'.includes(operator)){
+while (!operators.includes(operator)){
     operator = prompt("Enter operator:");
 }
 
-
-let sumUp = function (first, second) {
-    return first+second;
-}
-
-let substract = function (first, second) {
-    return first-second;
-}
-
-let multiply = function (first, second) {
-    return first*second;
-}
-
-let divide = function (first, second) {
-    return first/second;
-}
-
-switch (operator){
-    case "+" : {
-        alert(`Answer: ${sumUp(firstNumber, secondNumber)}`);
-        break;
-    }
-
-    case "-" : {
-        alert(`Answer: ${substract(firstNumber, secondNumber)}`);
-        break;
-    }
-
-    case "*" : {
-        alert(`Answer: ${multiply(firstNumber, secondNumber)}`);
-        break;
-    }
-
-    case ("/" || ":") : {
-        alert(`Answer: ${divide(firstNumber, secondNumber)}`);
-        break;
+function calculator(firstNumber, secondNumber, operator){
+    switch (operator) {
+        case "+" : {
+            return firstNumber+secondNumber;
+            break;
+        }
+        case '-' : {
+            return firstNumber-secondNumber;
+            break;
+        }
+        case '*' : {
+            return firstNumber*secondNumber;
+            break;
+        }
+        case ('/'||':') : {
+            return firstNumber/secondNumber;
+            break;
+        }
     }
 }
+
+console.log(calculator(+firstNumber, +secondNumber, operator));
