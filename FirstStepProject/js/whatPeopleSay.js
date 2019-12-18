7950d0e9ba87edb41262fac3a70653dbce749f42
Stan Bondar <bondar.stan@gmail.com>
2019-12-18 14:02:38 +0200

    finished carousel

--- FirstStepProject/js/whatPeopleSay.js
+++ FirstStepProject/js/whatPeopleSay.js
@@ -1,16 +1,81 @@
-let icons = document.querySelector('.icons-cover');
+let iconsCover = document.querySelector('.icons-cover');
+const icons = document.querySelectorAll('.carousel-icon');
 let iconsPrevTarget = document.querySelector('.active-icon');
 let iconsCoverPrevTarget = document.querySelector('.active-icon-cover');
 let mainIcon = document.querySelector('.what-say-person-icon');
+let description = document.querySelector('.what-people-say-description');
+let name = document.querySelector('.what-say-person-name');
+let position = document.querySelector('.what-say-person-position');
+const nextButton = document.querySelector('.next-button');
+const prevButton = document.querySelector('.previous-button');
 
-const iconsHandler = event => {
-    event.target.classList.add('active-icon');
-    event.target.parentNode.classList.add('active-icon-cover');
-    iconsPrevTarget.classList.remove('active-icon');
-    iconsCoverPrevTarget.classList.remove('active-icon-cover');
-    iconsCoverPrevTarget = event.target.parentNode;
-    iconsPrevTarget = event.target;
-    mainIcon.setAttribute('src', event.target.getAttribute('src'));
+const descriptions = ['Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.',
+    'Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance.',
+    'Integer dignissim, augue tempus ultricies luctus, quam dui laoreet sem, non dictum odio nisi quis massa. Morbi pulvinar odio eget aliquam facilisis. Tempus ultricies luctus, quam dui laoreet sem, non dictum odio nisi quis massa. Morbi pulvinar odio eget aliquam facilisis.',
+    'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don\'t look even slightly believable.'];
+
+const names = ['Nina Petrova', 'Man with jacket', 'HASAN ALI', 'Glasses blonde'];
+const positions = ['Photographer', 'Witcher', 'UX Designer', 'Accountant'];
+
+const changeIconsHandler = event => {
+    if(event.target.hasAttribute('src')) {
+        if (event.target !== iconsPrevTarget) {
+            event.target.classList.add('active-icon');
+            event.target.parentNode.classList.add('active-icon-cover');
+            iconsPrevTarget.classList.remove('active-icon');
+            iconsCoverPrevTarget.classList.remove('active-icon-cover');
+            iconsCoverPrevTarget = event.target.parentNode;
+            iconsPrevTarget = event.target;
+            mainIcon.setAttribute('src', event.target.getAttribute('src'));
+
+            description.innerText = descriptions[event.target.getAttribute('data-icon-number')];
+            name.innerText = names[event.target.getAttribute('data-icon-number')];
+            position.innerText = positions[event.target.getAttribute('data-icon-number')];
+        }
+    }
 };
 
-icons.addEventListener('click', iconsHandler);
+const setNextIconActive = ()=> {
+    let switcher = iconsPrevTarget.getAttribute('data-icon-number');
+    iconsPrevTarget.classList.remove('active-icon');
+    iconsPrevTarget.parentNode.classList.remove('active-icon-cover');
+    if(+switcher<icons.length-1){
+        switcher++;
+    }else if (+switcher === icons.length-1){
+        switcher = 0;
+    }
+    document.querySelector(`img[data-icon-number='${switcher}']`).classList.add('active-icon');
+    iconsPrevTarget = document.querySelector(`img[data-icon-number='${switcher}']`);
+    document.querySelector(`img[data-icon-number='${switcher}']`).parentNode.classList.add('active-icon-cover');
+    iconsCoverPrevTarget = document.querySelector(`img[data-icon-number='${switcher}']`).parentNode;
+    mainIcon.setAttribute('src', iconsPrevTarget.getAttribute('src'));
+
+    description.innerText = descriptions[switcher];
+    name.innerText = names[switcher];
+    position.innerText = positions[switcher];
+};
+
+const setPreviousIconActive = ()=> {
+    let switcher = iconsPrevTarget.getAttribute('data-icon-number');
+    iconsPrevTarget.classList.remove('active-icon');
+    iconsPrevTarget.parentNode.classList.remove('active-icon-cover');
+    if(+switcher>0){
+        switcher--;
+    }else if (+switcher === 0){
+        switcher = icons.length-1;
+    }
+    document.querySelector(`img[data-icon-number='${switcher}']`).classList.add('active-icon');
+    iconsPrevTarget = document.querySelector(`img[data-icon-number='${switcher}']`);
+    document.querySelector(`img[data-icon-number='${switcher}']`).parentNode.classList.add('active-icon-cover');
+    iconsCoverPrevTarget = document.querySelector(`img[data-icon-number='${switcher}']`).parentNode;
+    mainIcon.setAttribute('src', iconsPrevTarget.getAttribute('src'));
+
+    description.innerText = descriptions[switcher];
+    name.innerText = names[switcher];
+    position.innerText = positions[switcher];
+};
+
+iconsCover.addEventListener('click', changeIconsHandler);
+nextButton.addEventListener('click', setNextIconActive);
+prevButton.addEventListener('click', setPreviousIconActive);
+
