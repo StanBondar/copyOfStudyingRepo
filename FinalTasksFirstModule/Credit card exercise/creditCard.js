48dad81b72385905713614f6331a161a9c18151e
Stan Bondar <bondar.stan@gmail.com>
2019-12-24 14:31:31 +0200

    Finished "Credit card task" for myself

--- FinalTasksFirstModule/Credit card exercise/creditCard.js
+++ FinalTasksFirstModule/Credit card exercise/creditCard.js
@@ -1,6 +1,7 @@
 let pinCode = document.querySelector('#pin-code');
 let howManyCash = document.querySelector('#money-sum');
-const completeTransactionBtn = document.querySelector('get-cash');
+const completeTransactionBtn = document.querySelector('#get-cash');
+const result = document.querySelector('#cash-request-result');
 
 let creditCard = {
   amount: 10000,
@@ -14,7 +15,7 @@ let creditCard = {
         if (pinCode!==this.pin){
             this.attempts--;
             if(this.attempts>0){
-                return 'Неправильный пин-код! Попробуйте пожалуйста снова!';
+                return `Неправильный пин-код! Попробуйте пожалуйста снова! Осталось количество попыток: ${this.attempts}`;
             }else {
                 this.status = 'disabled';
                 return 'Неправильный пин-код! Вы исчерпали количество попыток. Ваша карта заблокирована, обратитесь в банк для ее разблокировки';
@@ -23,7 +24,7 @@ let creditCard = {
             this.attempts = 3;
             if(howManyCash<this.amount){
                 this.amount-=howManyCash;
-                return 'Получите ваши - сумма, которую человек хочет снять-';
+                return `Получите ваши ${howManyCash} сумма, которую человек хочет снять. Остаток: ${this.amount}`;
             }else {
                 return 'К сожалению, на вашем счету недостаточно средств';
             }
@@ -32,5 +33,5 @@ let creditCard = {
 };
 
 completeTransactionBtn.addEventListener('click', ()=>{
-    creditCard.getCash()
+    result.innerText = creditCard.getCash(+pinCode.value, +howManyCash.value);
 });
