29eaed09844c0088e482944814ee6a02212fe99d
Stan Bondar <bondar.stan@gmail.com>
2020-02-24 15:39:17 +0200

    added HW4

--- advancedJS/HW4/js/card.js
+++ advancedJS/HW4/js/card.js
@@ -1,19 +1,77 @@
 export default class Card {
-    constructor(parent, index){
-        this.parent = parent;
+    constructor(parentElement, index, parentObject) {
+        this.parentElement = parentElement;
+        this.parentObject = parentObject;
         this.index = index;
-        this.createCard(this.parent);
+        this.createCard(this.parentElement);
+        this.addEventlisteners();
     }
 
-    createCard(parent){
+    createCard(parentElement) {
         this.card = document.createElement('div');
         this.card.setAttribute('data-index', this.index);
+        this.card.setAttribute('draggable', 'true');
         this.card.classList.add('card');
+        // this.cardContent = document.createElement('textarea');
         this.cardContent = document.createElement('p');
         this.cardContent.classList.add('cardContent');
         this.cardContent.innerText = ' ';
         this.cardContent.setAttribute('contenteditable', 'true');
         this.card.append(this.cardContent);
-        parent.append(this.card);
+        parentElement.append(this.card);
+        this.cardContent.focus();
+        console.log(this.index);
+    }
+
+    addEventlisteners = () => {
+        console.log(this.parentObject);
+        this.parentObject.selected = null;
+
+        let dragOver = (e) => {
+            if (isBefore(this.parentObject.selected, e.target)) { 
+                e.target.parentNode.insertBefore(this.parentObject.selected, e.target) 
+            }else if(isBefore(this.parentObject.selected, e.target.parentNode)){
+                e.target.closest('.cards-cover').insertBefore(this.parentObject.selected, e.target.parentNode);
+            }
+            else{
+                if(e.target.classList.contains('card')){
+                    e.target.parentNode.insertBefore(this.parentObject.selected, e.target.nextSibling)
+                }else  e.target.closest('.cards-cover').insertBefore(this.parentObject.selected, e.target.parentNode.nextSibling);
+            }
+        }
+
+        let dragEnd = () => {
+            this.parentObject.selected = null
+        }
+
+        let dragStart = (e) => {
+            console.log('Drag start');
+            console.log(this);
+            e.dataTransfer.effectAllowed = "move";
+            e.dataTransfer.setData("text/plain", null);
+            this.parentObject.selected = e.target;
+            console.log(event.target);
+        }
+
+        let isBefore = (el1, el2) => {
+            let cur;
+            if (el2.parentNode === el1.parentNode) {
+                for (cur = el1.previousSibling; cur; cur = cur.previousSibling) {
+                    if (cur === el2) return true;
+                }
+            } else return false;
+        }
+
+        this.card.addEventListener('dragstart', function () {
+            console.log(this);
+            dragStart(event);
+        }.bind(this))
+        this.card.addEventListener('dragover', function () {
+            dragOver(event);
+        }.bind(this))
+        this.card.addEventListener('dragend', function () {
+            console.log(this);
+            dragEnd();
+        }.bind(this))
     }
 }
--- advancedJS/HW4/js/column.js
+++ advancedJS/HW4/js/column.js
@@ -1,18 +1,58 @@
-import Card from './card';
+import Card from './card.js';
 
-export default class Column {
+class Column {
     constructor(parent, index){
         this.parent = parent;
-        this.index = index;
+        this.cardIndex = index;
         this.cards = [];
         this.createColumn(this.parent);
+        this.defineEventListeners();
+    }
+
+    createNewCard(){
+        this.cards.push(new Card(this.cardsCover, this.cardIndex, this));
+        this.cardIndex++;
     }
 
     createColumn(parent){
         this.column = document.createElement('div');
         this.column.classList.add('column');
-        parent.append(this.column);
+        this.createNewCardBtn = document.createElement('button');
+        this.createNewCardBtn.classList.add('create-card-btn');
+        this.createNewCardBtn.innerText ='Create new card';
+
+        this.cardsCover = document.createElement('div');
+        this.cardsCover.classList.add('cards-cover');
+
+        this.sortBtn = document.createElement('button');
+        this.sortBtn.innerHTML = 'A-z';
+        this.sortBtn.classList.add('sort-btn');
+        
+
+        this.column.append(this.sortBtn, this.cardsCover, this.createNewCardBtn);
+        parent.prepend(this.column);
     }
 
-    createNewCard
+    defineEventListeners(){
+        this.createNewCardBtn.addEventListener('click', () => {
+            this.createNewCard();
+        })
+
+        this.sortBtn.addEventListener('click',()=>{
+            this.cards.sort((prev, next) => {
+                if ( prev.card.innerText < next.card.innerText ) return -1;
+                if ( prev.card.innerText < next.card.innerText ) return 1;
+            });
+            $('.cards-cover').fadeTo(400, 0.1,()=> {
+                $('.cards-cover').text('');
+                this.cards.forEach(element => {
+                    this.cardsCover.append(element.card);
+                });
+            });
+            $('.cards-cover').fadeTo(400, 1);
+        })
+    }
 }
+
+
+window.col = new Column(document.body, 1);
